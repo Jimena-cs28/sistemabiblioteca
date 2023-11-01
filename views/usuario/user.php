972c4d497e447f63b76adcc1e9bc2d61a61aafa1
kiarabq <kiarabeequispe18@gmail.com>
2023-11-01 16:05:41 -0500

    Update user.php

--- views/usuario/user.php
+++ views/usuario/user.php
@@ -33,7 +33,7 @@ if (!isset($_SESSION['login']) || !$_SESSION['login']['status']){
         <img class="img" src="../../img/logo2.png" alt="logo">
         <h6>"Una santanina, una señorita de éxito"</h6>
         <a class="btn btn-outline-dark" href=" ">Historial</a>
-        <a class="btn btn-outline-dark" href="../../controller/usuario.controller.php?operacion=destroy">Cerrar sesion</a>
+        <a class="btn btn-outline-dark" href="../../controller/usuario.controller.php?operacion=destroy">Cerrar sesión</a>
         <?php echo $_SESSION["login"]["nombres"]?>
 			</p>
 
